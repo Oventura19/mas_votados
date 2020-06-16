ef423901b0a78a9c9f8b0b06fb12af78659e466f
Oventura19 <47031203+Oventura19@users.noreply.github.com>
2020-06-16 17:01:16 -0500

    Updates

---
+++ precache-manifest.8544dd7345764e0f09fe64e55263e787.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef573b19174e5d06ca9dfa91ca62f20",
+    "url": "/mas_votados/index.html"
+  },
+  {
+    "revision": "02c0fd26a40012f94a61",
+    "url": "/mas_votados/static/css/main.cb86bf43.chunk.css"
+  },
+  {
+    "revision": "26ae042c5c06d0f34f2a",
+    "url": "/mas_votados/static/js/2.4e63bdcd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mas_votados/static/js/2.4e63bdcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02c0fd26a40012f94a61",
+    "url": "/mas_votados/static/js/main.0462d578.chunk.js"
+  },
+  {
+    "revision": "217689be3fa9852eaa43",
+    "url": "/mas_votados/static/js/runtime-main.4c1afdaa.js"
+  }
+]);
